8c7ac3b1d335980f55cd67ee9b94e2953aa967ab
DKhan123 <67632963+DKhan123@users.noreply.github.com>
2020-07-07 13:50:43 +0200

    Update Meemum_O.m

--- Optimisation Algorithm/Meemum_O.m
+++ Optimisation Algorithm/Meemum_O.m
@@ -1,7 +1,9 @@
- function [Result,Status]=Meemum_O(T,P)
-%% Run meemum.exe 
-% Generate executable file for meemum input: Name of dat file (BFCFs in this example), no to
-% changing bulk composition, Enter T & P, 0 0 to end meemum. 
+% Gibbs Free Energy Minimiser 
+% This function can be replaced with an alternative Gibbs energy minimiser. 
+% If replaced, read files must be updated. 
+function [Result,Status]=Meemum_O(T,P)
+
+% Format Pressure and Temperature to relevant units for minimiser [bars, K]
 Pb=P*1e4;
 TP = [T,Pb];
 TPn=zeros(numel(TP),1);
@@ -10,7 +12,10 @@ TPn(2*i-1)=TP(i,1);
 TPn(2*i)=TP(i,2);
 end
 
+% Generate executable file for meemum input
 executefile = fopen('executefilename.txt','w');
+% Input commands requested by minimiser: Name of dat file (BFCFs in this
+% example), no to changing bulk composition, Enter T & P, 0 0 to end meemum. 
 fprintf(executefile,[strcat('BFCFs','\n','n','\n')]);
 fprintf(executefile,'%d\n',TPn);
 fprintf(executefile,'%s\n','0','0');
